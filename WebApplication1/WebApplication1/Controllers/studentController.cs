49213f0fc3b3d6494f1b00cd1dd765703bacccd9
KIST Student3 <student3@sddb.student>
2022-03-14 09:23:41 +0545

    edit and delete

--- WebApplication1/WebApplication1/Controllers/studentController.cs
+++ WebApplication1/WebApplication1/Controllers/studentController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,11 +22,38 @@ namespace WebApplication1.Controllers
 
             return View();
         }
+        public ActionResult Edit(int id)
+        {
+            student data = db.students.Find(id);
+            return View(data);
+        }
         public ActionResult Savedata(student student)
         {
             db.students.Add(student);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult Updatedata(student student)
+        {
+           // student std = db.students.Find(student.sid);
+            db.Entry(student).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            student data = db.students.Find(id);
+            return View(data);
+        }
+        public ActionResult DeleteData(student student)
+        {
+            student delete = db.students.Find(student.sid);
+            db.students.Remove(delete);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
+       
 }
